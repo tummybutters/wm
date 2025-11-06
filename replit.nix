4cd028754f55af829302e9b71a99a45dd807b9b1
tummybutters <your_github_email@example.com>
2025-11-05 19:13:05 -0800

    Simplify Replit Nix configuration

--- replit.nix
+++ replit.nix
@@ -1,10 +1,6 @@
 { pkgs }: {
   deps = [
     pkgs.nodejs-20_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.nodePackages.prettier
-    pkgs.yarn
-    pkgs.replitPackages.jest
   ];
 }
 
